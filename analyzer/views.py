94e8872529e30acf261800dc73cbdd45aa083729
jonghkim <quantic.jh@gmail.com>
2018-03-03 20:53:06 +0900

    add price api

--- analyzer/views.py
+++ analyzer/views.py
@@ -7,10 +7,27 @@ import pymongo
 from pymongo import MongoClient
 import analyzer.server_config as config
 import datetime
+from django.http import JsonResponse
 # Create your views here.
-client = MongoClient(config.mongoserver, 27017, username=config.mongoid, password=config.mongopw)
-db_names = client.database_names()
 
 def index(request):
     return render(request, 'analyzer/index.html', {})
 
+def get_price(request):
+    client = MongoClient(config.mongoserver, 27017, username=config.mongoid, password=config.mongopw)
+    db_names = client.database_names()
+    context = {}
+
+    db = client.gekko
+    poloniex_prices = db.poloniex_prices
+    
+    price_data = {}
+    for poloniex_price in poloniex_prices.find().limit(20):
+        p_time = datetime.datetime.utcfromtimestamp(poloniex_price['start']).strftime('%Y-%m-%dT%H:%M:%SZ')
+        price_data[p_time] = poloniex_price['close'] 
+    
+    context['price_count'] =  poloniex_prices.count()
+    context['price_data'] =  price_data
+
+    #return HttpResponse(db_names[0])
+    return JsonResponse(context)
