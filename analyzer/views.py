91ef9c3d662c3f8ea0fc7949cf821ae5d1795689
jonghkim <quantic.jh@gmail.com>
2018-03-03 21:24:36 +0900

    price_data

--- analyzer/views.py
+++ analyzer/views.py
@@ -21,13 +21,28 @@ def get_price(request):
     db = client.gekko
     poloniex_prices = db.poloniex_prices
     
-    price_data = {}
-    for poloniex_price in poloniex_prices.find().limit(20):
+    price_data = []
+    for poloniex_price in poloniex_prices.find().sort('start',pymongo.DESCENDING).limit(100):
         p_time = datetime.datetime.utcfromtimestamp(poloniex_price['start']).strftime('%Y-%m-%dT%H:%M:%SZ')
-        price_data[p_time] = poloniex_price['close'] 
+        price_data.append([p_time,poloniex_price['close'],poloniex_price['start']])
+
+    min_price_time = price_data[-1][2]
+
+    poloniex_adivces = db.poloniex_advices
     
+    advice_data = []
+    for poloniex_advice in poloniex_adivces.find().sort('time',pymongo.DESCENDING):
+        p_time = datetime.datetime.utcfromtimestamp(poloniex_advice['time']).strftime('%Y-%m-%dT%H:%M:%SZ')
+        if min_price_time > poloniex_advice['time']:
+            break
+        advice_data.append([p_time,poloniex_advice['recommendation']])
+
+    context['min_price_time'] = min_price_time
+    context['min_price_time2'] = poloniex_advice['time']
+
     context['price_count'] =  poloniex_prices.count()
     context['price_data'] =  price_data
+    context['advice_data'] =  advice_data
 
     #return HttpResponse(db_names[0])
     return JsonResponse(context)
